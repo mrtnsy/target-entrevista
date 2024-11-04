6fa7d0e9c3eb62b346e3965cf206ed68800278f8
yurimartins01 <yuri.martins@sou.unaerp.edu.br>
2024-11-04 18:55:14 -0300

    Update 3_soma.js

--- 3_soma.js
+++ 3_soma.js
@@ -1,3 +1,15 @@
 //3) Observe o trecho de código abaixo: int INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE faça 
 //   { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA); Ao final do processamento, qual será o valor da variável SOMA?
 
+//Resposta: Ao final do processamento o valor da variável soma será: 77
+
+let indice = 12
+let soma = 0
+k = 1
+
+while(k < indice){
+    k++
+    soma += k
+}
+
+console.log(`Ao final do processamento o valor da variável soma será: ${soma}`)
